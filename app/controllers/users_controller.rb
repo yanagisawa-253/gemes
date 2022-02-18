006e1161ee7510d54ad5cab29e84bc6112a0a95a
yanagisawa-253 <anaguma253@gmail.com>
2022-02-18 09:31:56 +0000

    [update]controller記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update
+    redirect_to user_path(@user)
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :image)
+  end
 end
