a7cf11e342f2b2b4223db29897186bad5c0b9a2a
yanagisawa-253 <anaguma253@gmail.com>
2022-02-18 09:06:35 +0000

    [update]user.model作成完了

--- db/migrate/20220218084800_create_users.rb
+++ db/migrate/20220218084800_create_users.rb
@@ -1,6 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      t.string :name
+      t.text :introduction
+      t.string :image_id
 
       t.timestamps
     end
