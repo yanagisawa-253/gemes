90a6dc15bf740498bcddf8f77c2273e38103affc
yanagisawa-253 <anaguma253@gmail.com>
2022-02-19 13:12:45 +0000

    [update]編集ページ作成

--- app/models/custmer.rb
+++ app/models/custmer.rb
@@ -3,4 +3,6 @@ class Custmer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  
 end
