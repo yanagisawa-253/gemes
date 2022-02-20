55a578479aeb010546b90dcd582e5a6783eff1ca
yanagisawa-253 <anaguma253@gmail.com>
2022-02-20 11:47:17 +0000

    [update]レイアウト調整

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :custmers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :custmers
+  resources :tasks
 end
