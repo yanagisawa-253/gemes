ed3dfdad83b105d62d6770229393dc64ac9dcac4
yanagisawa-253 <anaguma253@gmail.com>
2022-02-21 08:25:28 +0000

    [update]モデルのアソシエーション

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,6 @@
 class Task < ApplicationRecord
+  belongs_to :user
+  
+  validates :task_name, :presence => { :message => 'タイトルを入力してください' }
+  validates :task_body, :presence => { :message => '本文を入力してください' }
 end
